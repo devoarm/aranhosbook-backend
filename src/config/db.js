d1fe5dc9388f7837315bc4dbfe3fa28616702856
devoarm <101738850+devoarm@users.noreply.github.com>
2022-11-25 11:56:46 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,7 @@
 const db_office = require("knex")({
   client: "mysql2",
   connection: {
-    // host: "192.168.2.7",
-    // port: 3306,
-    // user: "aranhos",
-    // password: "aranzjkowfh",
-    // database: "hosofficedb",
+    
     host: "localhost",
     port: 3306,
     user: "root",
